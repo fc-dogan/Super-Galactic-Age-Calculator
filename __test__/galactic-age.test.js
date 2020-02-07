cd5401184ab28cfacda8be2aff69b3197fee4d09
Fatma <fceyda@gmail.com>
2020-02-07 11:06:35 -0800

    add test to calculate age as a Jupiter age

--- __test__/galactic-age.test.js
+++ __test__/galactic-age.test.js
@@ -20,6 +20,11 @@ describe('GalacticAge', () => {
   test('should correctly calculate age as a Mars age', () => {
     var galacticAge = new GalacticAge(30);
     expect(galacticAge.marsYears()).toEqual(16);
+  });
+
+  test('should correctly calculate age as a Jupiter age', () => {
+    var galacticAge = new GalacticAge(30);
+    expect(galacticAge.jupiterYears()).toEqual(3);
   })
 
 
