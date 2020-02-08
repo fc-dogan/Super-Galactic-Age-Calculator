03e2d714ad05510cc935c064ac30c82bc5d55d71
Fatma <fceyda@gmail.com>
2020-02-08 15:02:55 -0800

    add years calculator val to main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,28 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { GalacticAge } from './galactic-age';
+
+var galacticage = new GalacticAge(age);
 
 $(document).ready(function() {
   $("#age-form").submit(function() {
     event.preventDefault();
-    var age = $("#age").val();
+    let age = $("#age").val();
+
+    let mercuryYears = galacticAge.mercuryYears();
+    let venusYears = galacticAge.venusYears();
+    let marsYears = galacticAge.marsYears();
+    let jupiterYears = galacticAge.jupiterYears();
+
+    $(".mercury-years").text(mercuryYears);
+    $(".venus-years").text(venusYears);
+    $(".mars-yers").text(marsYears);
+    $(".jupiter-years").text(jupiterYears);
     
 
   });
+
+  
 
 });
