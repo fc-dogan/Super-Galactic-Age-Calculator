0fbce2f92896a2db920e5f0bf949816c3db81544
Fatma <fceyda@gmail.com>
2020-02-10 19:40:04 -0800

    create lifeEXpectancy property in GalacticYearsLeft constructor

--- src/galactic-years-left.js
+++ src/galactic-years-left.js
@@ -1,27 +1,26 @@
 import { LifeExpectancy } from './../src/lifeExpectancy';
 
-let lifeExpectancy = new LifeExpectancy();
-
 export class GalacticYearsLeft {
   constructor(age, yearsLeft) {
     this.age = age;
     this.yearsLeft = yearsLeft;
+    this.lifeExpectancy = new LifeExpectancy();
   }
    
   checkYearsLeft() {
-    return this.yearsLeft = lifeExpectancy.avarageLife - this.age
+    return this.yearsLeft = this.lifeExpectancy.avarageLife - this.age
   }
 
   mercuryYearsLeft() {
-    return this.yearsLeft = Math.round( this.yearsLeft /= .24)
+    return this.yearsLeft = Math.round( this.yearsLeft / .24)
   }
 
   venusYearsLeft() {
-    return this.yearsLeft = Math.round(this.yearsLeft /= .62)
+    return this.yearsLeft = Math.round(this.yearsLeft / .62)
   }
 
   marsYearsLeft() {
-    return this.yearsLeft = Math.round(this.yearsLeft /= 1.88)
+    return this.yearsLeft = Math.round(this.yearsLeft / 1.88)
   }
   
   jupiterYearsLeft() {
